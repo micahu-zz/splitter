ddf7d6fe4a464ccc5d6cf7742f9831d8471ba317
micah <micah@atonomi.io>
2018-07-13 19:27:50 -0700

    Adds truffle migration scripts, npm scripts to package.json, some linting

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Splitter - Project #1
+
+```npm install```
+```npm run compile```
+```npm run ganache-cli```
+then in a new tab:
+```npm run deploy```
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+const Splitter = artifacts.require('Splitter')
+const SafeMath = artifacts.require('SafeMath')
+const init = require('../test/helpers/init')
+
+module.exports = (deployer, network, accounts) => {
+  const actors = init.getTestActorsContext(accounts)
+  const owner = actors.owner
+
+  deployer.deploy(SafeMath)
+  deployer.link(SafeMath, Splitter)
+  deployer.deploy(Splitter, actors.alice, actors.bob, actors.carol, {from: owner})
+}
